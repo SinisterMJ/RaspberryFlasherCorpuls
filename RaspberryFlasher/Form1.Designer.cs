802db5169ec767ecac9eedfd0852f9cb0664afa4
Anton Roth <anton.roth@corpuls.com>
2020-10-03 13:33:43 +0200

    Tool now uses proper command line utility to flash cards. Works with up to 6 cards

--- RaspberryFlasher/Form1.Designer.cs
+++ RaspberryFlasher/Form1.Designer.cs
@@ -30,49 +30,138 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBox_SerialNumber = new System.Windows.Forms.TextBox();
+            this.labelProductCode = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox_SerialNumber
             // 
             this.textBox_SerialNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox_SerialNumber.Location = new System.Drawing.Point(261, 75);
+            this.textBox_SerialNumber.Location = new System.Drawing.Point(436, 37);
+            this.textBox_SerialNumber.Margin = new System.Windows.Forms.Padding(4);
             this.textBox_SerialNumber.Multiline = true;
             this.textBox_SerialNumber.Name = "textBox_SerialNumber";
-            this.textBox_SerialNumber.Size = new System.Drawing.Size(407, 36);
+            this.textBox_SerialNumber.Size = new System.Drawing.Size(521, 43);
             this.textBox_SerialNumber.TabIndex = 0;
             this.textBox_SerialNumber.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_SerialNumber_KeyPress);
+            // 
+            // labelProductCode
+            // 
+            this.labelProductCode.AutoSize = true;
+            this.labelProductCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelProductCode.Location = new System.Drawing.Point(7, 40);
+            this.labelProductCode.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelProductCode.Name = "labelProductCode";
+            this.labelProductCode.Size = new System.Drawing.Size(258, 29);
+            this.labelProductCode.TabIndex = 1;
+            this.labelProductCode.Text = "Produktcode scannen";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(14, 78);
+            this.label1.Location = new System.Drawing.Point(7, 124);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(202, 25);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Produktcode scannen";
+            this.label1.Size = new System.Drawing.Size(243, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Currently installing...";
+            this.label1.Visible = false;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExit.Location = new System.Drawing.Point(12, 324);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(217, 49);
+            this.buttonExit.TabIndex = 3;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(14, 202);
+            this.label2.Location = new System.Drawing.Point(7, 171);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(186, 25);
-            this.label2.TabIndex = 2;
+            this.label2.Size = new System.Drawing.Size(243, 29);
+            this.label2.TabIndex = 4;
             this.label2.Text = "Currently installing...";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 220);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(243, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Currently installing...";
+            this.label3.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(431, 124);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(243, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Currently installing...";
+            this.label4.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(431, 171);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(243, 29);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Currently installing...";
+            this.label5.Visible = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(431, 220);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(243, 29);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Currently installing...";
+            this.label6.Visible = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(727, 313);
+            this.ClientSize = new System.Drawing.Size(969, 385);
+            this.ControlBox = false;
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonExit);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelProductCode);
             this.Controls.Add(this.textBox_SerialNumber);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "Form1";
@@ -84,8 +173,14 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox_SerialNumber;
+        private System.Windows.Forms.Label labelProductCode;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonExit;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- RaspberryFlasher/Form1.cs
+++ RaspberryFlasher/Form1.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
 using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Windows.Forms;
 
@@ -8,28 +12,85 @@ namespace RaspberryFlasher
     public partial class Form1 : Form
     {
         private Thread worker;
+        List<string> excludedDrives;
         public Form1()
         {
             InitializeComponent();
+            excludedDrives = new List<string>();
+            string listAll = ConfigurationManager.AppSettings["Exclude_Drives"];
+            foreach (string s in listAll.Split(' '))
+                excludedDrives.Add(s);            
         }
 
-        void setTextBoxText(string newText)
+        void SetTextBoxText(string newText)
         {
             this.UIThreadAsync(delegate
             {
                 textBox_SerialNumber.Text = newText;
+                Refresh();
             });
         }
 
-        void setTextBoxEnabled(bool enabled)
+        void SetTextBoxEnabled(bool enabled)
         {
             this.UIThreadAsync(delegate
             {
-                this.textBox_SerialNumber.Enabled = enabled;
+                textBox_SerialNumber.Enabled = enabled;
+                Refresh();
             });
         }
 
-        private bool getColorCode(out string colorCode)
+        void SetTextBoxFocus()
+        {
+            this.UIThreadAsync(delegate
+            {
+                textBox_SerialNumber.Focus();
+            });
+        }
+
+        void SetLabelText(string text, int id)
+        {
+            string labelName = "label" + id.ToString();
+
+            foreach(Control c in this.Controls)
+            {
+                if (c.Name == labelName)
+                {
+                    this.UIThreadAsync(delegate
+                    {
+                        c.Text = text;
+                        Refresh();
+                    });
+                }
+            }
+        }
+
+        void SetLabelVisibility(bool enabled, int id)
+        {
+            string labelName = "label" + id.ToString();
+
+            foreach (Control c in Controls)
+            {
+                if (c.Name == labelName)
+                {
+                    this.UIThreadAsync(delegate
+                    {
+                        c.Visible = enabled;
+                        Refresh();
+                    });
+                }
+            }
+        }
+
+        void SetButtonExitEnabled(bool enabled)
+        {
+            this.UIThreadAsync(delegate
+            {
+                buttonExit.Enabled = enabled;
+            });
+        }
+
+        private bool GetColorCode(out string colorCode)
         {
             colorCode = "";
             string productCode = textBox_SerialNumber.Text;
@@ -61,44 +122,147 @@ namespace RaspberryFlasher
             return colorCode != "";
         }
 
-        void runCommand(string colorCode)
+        List<string> DriveLetters()
         {
-            //* Create your Process
+            List<string> result = new List<string>();
             Process process = new Process();
-            process.StartInfo.FileName = "cmd.exe";
-            process.StartInfo.Arguments = "/c DIR";
+            process.StartInfo.FileName = "wmic";
+            process.StartInfo.Arguments = "logicaldisk where drivetype=2 get Caption,Size";
             process.StartInfo.UseShellExecute = false;
             process.StartInfo.RedirectStandardOutput = true;
             process.StartInfo.RedirectStandardError = true;
             //* Set your output and error (asynchronous) handlers
-            process.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
-            process.ErrorDataReceived += new DataReceivedEventHandler(OutputHandler);
+            /*process.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
+            process.ErrorDataReceived += new DataReceivedEventHandler(OutputHandler);*/
             //* Start process and handlers
             process.Start();
-            process.BeginOutputReadLine();
-            process.BeginErrorReadLine();
+            StreamReader reader = process.StandardOutput;
+            string output = reader.ReadToEnd();
             process.WaitForExit();
-            setTextBoxEnabled(true);
+
+            Regex rx = new Regex(@".?(.):.*?.*?", RegexOptions.Compiled);
+            MatchCollection matches = rx.Matches(output);
+
+            foreach (Match match in matches)
+            {
+                GroupCollection groups = match.Groups;
+                if (excludedDrives.Contains(groups[1].Value))
+                    continue;
+
+                result.Add(groups[1].Value);
+            }
+
+            return result;
         }
 
-        void OutputHandler(object sendingProcess, DataReceivedEventArgs outLine)
+        void FlashImage(string imgPath, string letterPath, int id)
         {
-            //* Do your stuff with the output (write to console/log/StringBuilder)
-            Console.WriteLine(outLine.Data);
+            Process process = new Process();
+            process.StartInfo.FileName = ConfigurationManager.AppSettings["CLI.Tool"];
+            process.StartInfo.Arguments = imgPath + " " + letterPath;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            process.StartInfo.CreateNoWindow = true;
+
+            //process.OutputDataReceived += new DataReceivedEventHandler(OutputHandler);
+
+            process.OutputDataReceived += 
+                (Object _sender, DataReceivedEventArgs _args) =>
+                    OutputHandler(id, _sender, _args);
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.WaitForExit();
+            SetLabelText("SD Karte " + id.ToString() + ": Beendet.", id);
+            if (process.ExitCode != 0)
+            {
+                SetLabelText("SD Karte " + id.ToString() + ": Fehlerhaft. Fehlercode " + process.ExitCode.ToString(), id); ;
+            }
+        }
+
+        void RunCommand(string colorCode)
+        {
+            for (int i = 1; i <= 6; ++i)
+            {
+                SetLabelVisibility(false, i);
+                SetLabelText("", i);
+            }
+
+            //* Create your Process
+            List<string> drives = DriveLetters();
+
+            if (drives.Count < 6)
+            {
+                if (MessageBox.Show("Weniger als 6 SD-Karten gefunden. Fortfahren?", "SD-Karten Anzahl", MessageBoxButtons.YesNo) == DialogResult.No)
+                {
+                    ResetForm();
+                    return;
+                }
+            }
+
+            string basePath = ConfigurationManager.AppSettings["ImageFolder"];
+            string version = ConfigurationManager.AppSettings["Image_Version"];
+            string fileName = basePath + "Corpuls_Simulation_" + colorCode + "_" + version + ".img";
+            int count = 1;
+            List<Thread> allThreads = new List<Thread>();
+
+            foreach (string drive in drives)
+            {
+                SetLabelVisibility(true, count);
+                Thread local = new Thread(() => FlashImage(fileName, drive, count));
+                local.Start();
+                allThreads.Add(local);
+                Thread.Sleep(100);
+                count++;
+            }
+                        
+            while(true)
+            {
+                bool stop = true;
+                foreach(Thread t in allThreads)
+                {
+                    stop = stop && (!t.IsAlive);
+                }
+
+                if (stop)
+                    break;
+            }
+
+            ResetForm();
+        }
+
+        void ResetForm()
+        {
+            SetTextBoxEnabled(true);
+            SetButtonExitEnabled(true);
+            SetTextBoxText("");
+            SetTextBoxFocus();
+        }
+        void OutputHandler(int id, object sendingProcess, DataReceivedEventArgs outLine)
+        {
+            if (outLine.Data != null)
+            {
+                string[] bytes = outLine.Data.Split('/');
+                Int64 percent = Convert.ToInt64(bytes[0]) * 100 / Convert.ToInt64(bytes[1]);
+                SetLabelText("SD Karte " + id.ToString() + ": " + percent.ToString("D" + 2) + "% fertig", id);
+            }
         }
 
         private void workingThread()
         {
             string color;
-            if (!getColorCode(out color))
+            if (!GetColorCode(out color))
             {
-                setTextBoxText("");
                 MessageBox.Show("Ungültiger Produktcode. Bitte erneut scannen.");
+                ResetForm();
                 return;
             }
 
-            setTextBoxEnabled(false);
-            runCommand(color);
+            SetTextBoxEnabled(false);
+            SetButtonExitEnabled(false);
+            RunCommand(color);
         }
 
         private void textBox_SerialNumber_KeyPress(object sender, KeyPressEventArgs e)
@@ -110,6 +274,11 @@ namespace RaspberryFlasher
                 worker = new Thread(work);
                 worker.Start();
             }
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
